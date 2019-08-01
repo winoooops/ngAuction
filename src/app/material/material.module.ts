d4effe46fbf94b35a66c374146a8dcfce76d04ed
weiwang <--add>
2019-08-01 11:21:21 +1000

    created material feature module to app module

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatToolbarModule, MatButtonModule, MatIconModule } from '@angular/material'
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+  ],
+  exports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule
+  ]
+})
+export class MaterialModule { }
