c7d0ab40a292073647da0fdfcee80137fc0cff2e
weiwang <--add>
2019-08-01 11:59:23 +1000

    adding toolbar barebone

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatToolbarModule, MatButtonModule, MatIconModule } from '@angular/material'
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatButtonModule } from '@angular/material/button'
+import { MatIconModule } from '@angular/material/icon'
 // import { FlexLayoutModule } from '@angular/flex-layout'
 
 
