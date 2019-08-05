ccae027d5377f83c93dd16632423be1cd962f535
weiwang <winoooops@outlook.com>
2019-08-05 19:52:39 +1000

    inject the producSerivice in the home componnet...

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { MediaObserver, MediaChange } from '@angular/flex-layout'
+import { Observable, Subscription } from 'rxjs'
+import { map } from 'rxjs/operators'
+
+import { ProductService, Product } from '../shared/services/product.service';
 
 @Component({
   selector: 'nga-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
+  readonly products$: Observable<Product[]>
 
-  constructor() { }
+  // on xs viewport, only plays 1 column 
+  // ......
+  readonly breakpointsToColumnsNumber = new Map([
+    ['xs', 1],
+    ['sm', 2],
+    ['md', 3],
+    ['lg', 4],
+    ['xl', 5]
+  ])
 
-  ngOnInit() {
+  constructor(
+    private productService: ProductService,
+  ) {
+
   }
 
 }
