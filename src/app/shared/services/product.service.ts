455c77d4611be779d4e10093b1d40cb0e73e3023
weiwang <winoooops@outlook.com>
2019-08-05 18:59:41 +1000

    defined and export the product interface...

--- src/app/shared/services/product.service.ts
+++ src/app/shared/services/product.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
 
+
+export interface Product {
+  id: number,
+  title: string,
+  price: number,
+  imageUrl: string
+  description: string
+}
+
 @Injectable({
   providedIn: 'root'
 })
-export class ProductService {
 
+export class ProductService {
   constructor() { }
 }
